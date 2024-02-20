fc5d62cad57b9ed60d2ebbf79560892be82718c5
Chinedu Friday <chinedukings2013@yahoo.com>
2024-02-20 22:36:43 +0100

    Add user_inputs.py file

---
+++ user_inputs.py
@@ -0,0 +1,5 @@
+name = input('Please enter your name: ')
+age = input('Please enter your age: ')
+location = input('Please enter your current location: ')
+u = 'Hello ' + name + ' you are ' + age + ' years old and you live in ' + location + '.'
+print(u)
